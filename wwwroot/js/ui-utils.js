a271714511aa9f3128fed9a57f7240850aab17f0
sametcn99 <sametcn99@gmail.com>
2026-02-19 06:51:30 +0300

    chore: clean up empty code change sections in the changes log

--- wwwroot/js/ui-utils.js
+++ wwwroot/js/ui-utils.js
@@ -24,3 +24,12 @@ window.unlockBodyScroll = () => {
     document.documentElement.style.overflow = "";
   }
 };
+
+window.downloadFile = (url, fileName) => {
+  const link = document.createElement("a");
+  link.href = url;
+  link.download = fileName;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+};
