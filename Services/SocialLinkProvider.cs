c10117325f1358660f7d252b12a12ff944b27a07
sametcn99 <sametcn99@gmail.com>
2026-05-18 01:40:49 +0300

    .

--- Services/SocialLinkProvider.cs
+++ Services/SocialLinkProvider.cs
@@ -59,7 +59,52 @@ public sealed class SocialLinkProvider : ISocialLinkProvider
             External = true,
             Category = "Development Platforms",
             IconColor = "#FFA116",
-        }
+        },
+                new()
+        {
+            Type = ["telegram", "tg"],
+            Link = "https://t.me/sametc0",
+            Label = "Telegram",
+            External = true,
+            Category = "Contact",
+            IconColor = "#26A5E4",
+        },
+        new()
+        {
+            Type = ["mail", "email", "gmail", "e-mail"],
+            Link = "mailto:sametcn99@gmail.com",
+            Label = "Mail",
+            External = false,
+            Category = "Contact",
+            IconColor = "#EA4335",
+        },
+        new()
+        {
+            Type = ["cv", "ozgecmis", "letter", "resume"],
+            Link = "/cv",
+            Label = "Resume",
+            External = false,
+            Category = "Professional Networks",
+            IconColor = "#1A73E8",
+        },
+        new()
+        {
+            Type = ["readme", "about"],
+            Link = "/readme",
+            Label = "Readme",
+            External = false,
+            Category = "Development Platforms",
+            IconColor = "#24292F",
+        },
+        new()
+        {
+            Type = ["support", "sponsor", "donate"],
+            Link = "/support",
+            Label = "Support Me",
+            External = false,
+            Category = "Contact",
+            IconColor = "#FFDD00",
+        },
     ];
 
     private static readonly Dictionary<string, int> CategoryOrder = new()
