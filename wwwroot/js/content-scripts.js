f01fdfcf9af63b96fe2d35520e0e160201549293
sametcn99 <sametcn99@gmail.com>
2026-06-02 22:42:38 +0300

    .

--- wwwroot/js/content-scripts.js
+++ wwwroot/js/content-scripts.js
@@ -297,7 +297,7 @@ window.renderMonacoEditor = async (containerId, code, language) => {
         'editorLineNumber.foreground': '#9ea8b7',    // --color-text-secondary
         'editor.selectionBackground': '#8ecbff38',   // --color-primary with opacity
         'editor.lineHighlightBackground': '#ffffff0a',
-        'ap.background': '#171c25',
+        'minimap.background': '#171c25',
         'scrollbarSlider.background': '#8ecbff15',
         'scrollbarSlider.hoverBackground': '#8ecbff30',
         'scrollbarSlider.activeBackground': '#8ecbff50'
@@ -312,7 +312,7 @@ window.renderMonacoEditor = async (containerId, code, language) => {
       language: monacoLang,
       theme: "personal-website-theme",
       readOnly: true,
-      ap: { enabled: false },
+      minimap: { enabled: false },
       fontFamily: '"JetBrains Mono", Consolas, Monaco, "Courier New", monospace',
       fontLigatures: true,
       fontSize: 14,
